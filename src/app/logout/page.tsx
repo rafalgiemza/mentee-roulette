275e02a9e4df60dcb7cb640cc2744f139a93c453
Rafał Giemza <rafal.giemza@softgarden.de>
2023-12-07 12:24:16 +0100

    logout dummy page

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <div className="py-10">Logout page</div>
+    </main>
+  );
+}
